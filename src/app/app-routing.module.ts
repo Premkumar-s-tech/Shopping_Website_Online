38cec9286a1ae8a2c176426805578723bc1a8024
sheninthjr <sheninthjr23@gmail.com>
2023-08-02 23:10:47 +0530

    Shopping Website Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path:'home',component:HomeContentComponent},
+  {path:'add-product',component:AddProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home-content/home-content.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-content',
+  templateUrl: './home-content.component.html',
+  styleUrls: ['./home-content.component.css']
+})
+export class HomeContentComponent {
+  reqdata = [{
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  },
+  {
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  },
+  {
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  },
+  {
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  },
+  {
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  },
+  {
+    "img":"assets/shirt.webp",
+    "title":"T-Shirt",
+    "price":"525",
+    "des":"Modern T-Shirt"
+  }
+]
+}
